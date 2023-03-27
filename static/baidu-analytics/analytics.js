1bd196a4554b352a945fe7092eaed82d4e6d11b1
yanxiaodi <929213769@qq.com>
2023-03-27 19:50:00 +0800

    update ready

---
+++ static/baidu-analytics/analytics.js
@@ -0,0 +1,8 @@
+// 百度统计
+var _hmt = _hmt || []
+;(function () {
+  const hm = document.createElement("script")
+  hm.src = "https://hm.baidu.com/hm.js?7267ca88f14bdb65d7e15a842d8be5e5"
+  var s = document.getElementsByTagName("script")[0]
+  s.parentNode.insertBefore(hm, s)
+})()
